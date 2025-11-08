3f911ab289287106979745efdba03e6559c1db3f
seoyoon lee <leeseoyoon01@gmail.com>
2025-11-08 12:55:42 +0900

    feat: main화면 레이아웃

--- app/src/main/java/com/example/wooah_tech/MainActivity.java
+++ app/src/main/java/com/example/wooah_tech/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.wooah_tech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
